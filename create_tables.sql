43551decc68e0b06f131e2f7e662bbe1d6cf5c16
Pavlo Karas <kar.pavel123@gmail.com>
2023-05-01 16:27:11 +0300

    Add checking if a table exist and gave the id a better naming

--- create_tables.sql
+++ create_tables.sql
@@ -1,9 +1,39 @@
 USE automation_workplace;
 GO
 
+IF EXISTS (SELECT *
+           FROM sys.tables
+           WHERE name = 'Journal')
+    DROP TABLE Journal;
+
+IF EXISTS (SELECT *
+           FROM sys.tables
+           WHERE name = 'Violation')
+    DROP TABLE Violation;
+
+IF EXISTS (SELECT *
+           FROM sys.tables
+           WHERE name = 'InspectionObject')
+    DROP TABLE InspectionObject;
+
+IF EXISTS (SELECT *
+           FROM sys.tables
+           WHERE name = 'Employee')
+    DROP TABLE Employee;
+
+IF EXISTS (SELECT *
+           FROM sys.tables
+           WHERE name = 'ResponsiblePerson')
+    DROP TABLE ResponsiblePerson;
+
+IF EXISTS (SELECT *
+           FROM sys.tables
+           WHERE name = 'Person')
+    DROP TABLE Person;
+
 CREATE TABLE Person
 (
-    ID          INT PRIMARY KEY IDENTITY (1,1),
+    ID_Person   CHAR(20) PRIMARY KEY,
     LastName    NVARCHAR(50)         NOT NULL,
     FirstName   NVARCHAR(50)         NOT NULL,
     MiddleName  NVARCHAR(50),
@@ -14,40 +44,40 @@ CREATE TABLE Person
 
 CREATE TABLE ResponsiblePerson
 (
-    ID INT PRIMARY KEY,
-    FOREIGN KEY (ID) REFERENCES Person (ID)
+    ID_ResponsiblePerson CHAR(20) PRIMARY KEY,
+    FOREIGN KEY (ID_ResponsiblePerson) REFERENCES Person (ID_Person)
 );
 
 CREATE TABLE Employee
 (
-    ID INT PRIMARY KEY,
-    FOREIGN KEY (ID) REFERENCES Person (ID)
+    ID_Employee CHAR(20) PRIMARY KEY,
+    FOREIGN KEY (ID_Employee) REFERENCES Person (ID_Person)
 );
 
 CREATE TABLE InspectionObject
 (
-    ID      INT PRIMARY KEY IDENTITY (1,1),
-    Name    NVARCHAR(100) NOT NULL,
-    Address NVARCHAR(200) NOT NULL,
-    Type    NVARCHAR(50)  NOT NULL
+    ID_InspectionObject CHAR(20) PRIMARY KEY,
+    Name                NVARCHAR(100) NOT NULL,
+    Address             NVARCHAR(200) NOT NULL,
+    Type                NVARCHAR(50)  NOT NULL
 );
 
 CREATE TABLE Violation
 (
-    ID          INT PRIMARY KEY IDENTITY (1,1),
-    Description NVARCHAR(MAX) NOT NULL
+    ID_Violation CHAR(20) PRIMARY KEY,
+    Description  NVARCHAR(MAX) NOT NULL
 );
 
 CREATE TABLE Journal
 (
-    ID                  INT PRIMARY KEY IDENTITY (1,1),
-    ObjectID            INT  NOT NULL,
-    EmployeeID          INT  NOT NULL,
-    ResponsiblePersonID INT  NOT NULL,
-    ViolationID         INT  NOT NULL,
-    RecordDate          DATE NOT NULL DEFAULT GETDATE(),
-    FOREIGN KEY (ObjectID) REFERENCES InspectionObject (ID),
-    FOREIGN KEY (EmployeeID) REFERENCES Employee (ID),
-    FOREIGN KEY (ResponsiblePersonID) REFERENCES ResponsiblePerson (ID),
-    FOREIGN KEY (ViolationID) REFERENCES Violation (ID)
+    ID_Journal          CHAR(20) PRIMARY KEY,
+    ObjectID            CHAR(20) NOT NULL,
+    EmployeeID          CHAR(20) NOT NULL,
+    ResponsiblePersonID CHAR(20) NOT NULL,
+    ViolationID         CHAR(20) NOT NULL,
+    RecordDate          DATE     NOT NULL DEFAULT GETDATE(),
+    FOREIGN KEY (ObjectID) REFERENCES InspectionObject (ID_InspectionObject),
+    FOREIGN KEY (EmployeeID) REFERENCES Employee (ID_Employee),
+    FOREIGN KEY (ResponsiblePersonID) REFERENCES ResponsiblePerson (ID_ResponsiblePerson),
+    FOREIGN KEY (ViolationID) REFERENCES Violation (ID_Violation)
 );
