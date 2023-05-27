1befc5f3f501c01121c610d44e4cde51b9d3b143
Pavlo Karas <kar.pavel123@gmail.com>
2023-05-27 13:57:28 +0300

    Update app configs

---
+++ my-app/renderer/env.mjs
@@ -0,0 +1,5 @@
+export const env = {
+    DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_USER: process.env.DATABASE_USER,
+    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD
+};
---
+++ my-app/renderer/next.config.mjs
@@ -0,0 +1,9 @@
+await import("./env.mjs");
+const config = {
+    reactStrictMode: true,
+    i18n: {
+        locales: ["en"],
+        defaultLocale: "en",
+    },
+};
+export default config;
