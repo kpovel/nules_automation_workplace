1804b124e027952723026229d9724f3a1721f7ac
Pavlo Karas <kar.pavel123@gmail.com>
2023-05-31 12:24:27 +0300

    Update grants

--- grants.sql
+++ grants.sql
@@ -4,14 +4,17 @@ CREATE LOGIN AdminLogin WITH PASSWORD = 'ComplexAdminPassword1!';
 CREATE USER AdminUser FOR LOGIN AdminLogin;
 GRANT ALL TO AdminUser;
 
-CREATE LOGIN InspectorLogin WITH PASSWORD = 'ComplexInspectorPassword1!';
-CREATE USER InspectorUser FOR LOGIN InspectorLogin;
-GRANT SELECT, INSERT, UPDATE ON Person TO InspectorUser;
-GRANT SELECT, INSERT, UPDATE ON Employee TO InspectorUser;
-GRANT SELECT, INSERT ON Journal TO InspectorUser;
-GRANT SELECT ON InspectionObject TO InspectorUser;
-GRANT SELECT ON Violation TO InspectorUser;
-
 CREATE LOGIN ResponsibleLogin WITH PASSWORD = 'ComplexResponsiblePassword1!';
 CREATE USER ResponsibleUser FOR LOGIN ResponsibleLogin;
-GRANT SELECT ON Journal TO ResponsibleUser;
+GRANT SELECT, INSERT, UPDATE ON Journal TO ResponsibleUser;
+GRANT SELECT ON Person TO ResponsibleUser;
+GRANT SELECT ON ResponsiblePerson TO ResponsibleUser;
+GRANT SELECT ON Employee TO ResponsibleUser;
+GRANT SELECT ON InspectionObject TO ResponsibleUser;
+GRANT SELECT ON Violation TO ResponsibleUser;
+
+CREATE LOGIN EmployeeLogin WITH PASSWORD = 'ComplexEmployeePassword1!';
+CREATE USER EmployeeUser FOR LOGIN EmployeeLogin;
+GRANT SELECT, INSERT ON Journal TO EmployeeUser
+GRANT SELECT ON InspectionObject TO EmployeeUser;
+GRANT SELECT ON Violation TO EmployeeUser;
